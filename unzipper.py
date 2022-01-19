c2eef40d7422aeb9e1df9d29fb6f0671e3a6aabe
uriri <uribo6165@gmail.com>
2022-01-19 17:02:14 +0900

    fix: スクリプト実行したディレクトリにzipが解凍されてしまう不具合修正

--- unzipper.py
+++ unzipper.py
@@ -24,19 +24,21 @@ class UnzipService:
 
         self.logger = logger
 
-    def _is_need_directory(self, unzip_dir: str, sample_file: str) -> bool:
+    def _is_need_directory(self, zip_file: Path, sample_file: str) -> bool:
         """ディレクトリを作成する必要があるか判定
 
         directory/file.* の形式になっているか確認
 
         Args:
-            unzip_dir (str): 解凍先ディレクトリ（zipファイル名）
+            zip_file (Path): 解凍先ディレクトリ（zipファイル名）
             sample_file (str): zipファイル内のファイルパス
 
         Returns:
             bool: ディレクトリ作成が必要
         """
-        return sample_file.startswith(unzip_dir + "/") is False
+        self.logger.debug(f"zip_file: {zip_file}")
+        self.logger.debug(f"sample_file: {sample_file}")
+        return sample_file.startswith(zip_file.stem + "/") is False
 
     def unzip(self, root_dir: Path, is_recursive: bool) -> None:
         """zip解凍
@@ -49,17 +51,18 @@ class UnzipService:
 
         for zip_file in root_dir.glob(ptn):
             with zipfile.ZipFile(zip_file) as zf:
-                self.logger.info(f"unzip: {zf.filename}")
+                zip_file = Path(zf.filename)
+                self.logger.info(f"unzip: {zip_file}")
 
-                unzip_dir, _ = zf.filename.split(".")
                 sample_file = zf.namelist()[0]
-                is_need_dir = self._is_need_directory(unzip_dir, sample_file)
+                is_need_dir = self._is_need_directory(zip_file, sample_file)
 
+                unzip_dir = zip_file.parent
                 if is_need_dir:
+                    unzip_dir /= zip_file.stem
                     self.logger.info(f"create dir {unzip_dir}")
-                    zf.extractall(unzip_dir)
-                else:
-                    zf.extractall()
+                self.logger.info(f"unzip dir: {unzip_dir}")
+                zf.extractall(unzip_dir)
 
 
 if __name__ == "__main__":
